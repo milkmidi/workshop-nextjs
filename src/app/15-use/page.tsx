f36e053ab7b39e0ac6025d778d4fe9b193e1290d
milkmidi <milkmidi.liao@positivegrid.com>
2023-02-19 21:43:10 +0800

    feat: add 15

---
+++ src/app/15-use/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { use } from 'react';
+
+async function getData() {
+  // You would usually fetch data from an API here.
+  console.log('dashboard-client getData');
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1', { next: { revalidate: 60 } });
+  const data = await res.json();
+
+  // return data; // 這樣寫會無限回圈
+  // return `Dashboard data in Client Components, ${data.title}`;
+  return JSON.stringify(data);
+}
+
+const Page = () => {
+  const name = use(getData());
+  return <p>{name}!</p>;
+};
+export default Page;
