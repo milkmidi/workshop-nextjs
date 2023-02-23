bd58f88b568e9618d348b075cd95c6f6987dabb6
milkmidi <milkmidi.liao@positivegrid.com>
2023-02-23 17:32:35 +0800

    feat: redux

---
+++ 05-react-redux/src/components/App.tsx
@@ -0,0 +1,14 @@
+import MyCount from './MyCount';
+import MyCountButton from './MyCountButton';
+import MyCount2 from './MyCount2';
+
+const App = () => {
+  return (
+    <div className="app">
+      <MyCount />
+      <MyCount2 />
+      <MyCountButton />
+    </div>
+  );
+};
+export default App;
