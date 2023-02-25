3ad7bb9171d059551ab99c0363705c812f52a6c0
milkmidi <milkmidi.liao@positivegrid.com>
2023-02-25 09:12:16 +0800

    feat: route example

--- 01-examples/src/app/api/api-route/route.ts
+++ 01-examples/src/app/api/api-route/route.ts
@@ -1,13 +1,21 @@
 import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+// https://beta.nextjs.org/docs/api-reference/segment-config#fetchcache
+// export const fetchCache = 'force-no-store'; // 這個不知為何無效
 
 export async function GET(request: Request) {
+  const cookieStore = cookies();
+  const token = cookieStore.get('JWT_TOKEN');
+
   const { searchParams } = new URL(request.url);
   const value = searchParams.get('value');
 
   const res = await fetch(`https://milkmidi.vercel.app/api/mock?delay=1&value=${value}`, {
-    next: { revalidate: 20 }, // Revalidate every 20 seconds
+    // next: { revalidate: 20 }, // Revalidate every 20 seconds
+    cache: 'no-store',
   });
   const data = await res.json();
   // return Response.json({ data }); // TS Error https://beta.nextjs.org/docs/routing/route-handlers
-  return NextResponse.json({ data });
+  return NextResponse.json({ ...data, token });
 }
